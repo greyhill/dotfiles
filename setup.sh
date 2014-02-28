a14b9b554cc3d9673ef7d788c291ce7d401d3df2
Madison McGaffin <greyhill@gmail.com>
2014-02-28 11:01:27 -0500

    added zshrc

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ ln -sf `pwd`/dot_pentadactylrc ~/.pentadactylrc
 ln -sf `pwd`/dot_gitconfig ~/.gitconfig
 ln -sf `pwd`/dot_vimrc ~/.vimrc
 ln -sf `pwd`/dot_screenrc ~/.screenrc
+ln -sf `pwd`/dot_zshrc ~/.zshrc
