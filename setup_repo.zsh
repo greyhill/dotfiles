c5b3bc658db163d6150d8a2420791a99e9b66aa6
Madison McGaffin <greyhill@gmail.com>
2014-06-23 10:52:31 -0600

    added sckli

--- setup_repo.zsh
+++ setup_repo.zsh
@@ -26,4 +26,5 @@
 [[ ! -e bib ]] && git clone ssh://ec2/media/git/bib
 [[ ! -e ct_data ]] && git clone ssh://ir55/y/mcgaffin/repo/ct_data
 [[ ! -e gerecon ]] && git clone ssh://ec2/media/git/gerecon
+[[ ! -e sckli ]] && git clone ssh://ec2/media/git/sckli
 
