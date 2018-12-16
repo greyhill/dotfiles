132fe4b7da5b4f02093e27ca97305b45fd1ef357
Madison McGaffin <greyhill@gmail.com>
2018-12-16 14:28:33 -0800

    Adds syncProjects

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
-mkdir ~/bin
 ln -sf `pwd`/dot_gitconfig ~/.gitconfig
 ln -sf `pwd`/dot_vimrc ~/.vimrc
 ln -sf `pwd`/dot_screenrc ~/.screenrc
 ln -sf `pwd`/dot_zshrc ~/.zshrc
 ln -sf `pwd`/dot_ssh ~/.ssh/config
+
+# sync scripts
+if [ ! -d $HOME/bin ]; then
+    mkdir ~/bin
+fi
 ln -sf `pwd`/syncMusic ~/bin/syncMusic
+ln -sf `pwd`/syncProjects ~/bin/syncProjects
