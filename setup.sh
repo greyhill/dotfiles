26aa728bb85b6da6524721bc86eee8d12be4dbd7
Madison McGaffin <greyhill@gmail.com>
2018-12-16 13:44:01 -0800

    Adds syncMusic script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-ln -sf `pwd`/dot_pentadactylrc ~/.pentadactylrc
+mkdir ~/bin
 ln -sf `pwd`/dot_gitconfig ~/.gitconfig
 ln -sf `pwd`/dot_vimrc ~/.vimrc
 ln -sf `pwd`/dot_screenrc ~/.screenrc
 ln -sf `pwd`/dot_zshrc ~/.zshrc
 ln -sf `pwd`/dot_ssh ~/.ssh/config
+ln -sf `pwd`/syncMusic ~/bin/syncMusic
